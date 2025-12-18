9d4943b510b69af96b662c73bdcb94e045865b4b
suedakca <akcasueda@gmail.com>
2025-12-18 15:27:01 +0300

    değişiklikler yapıldı

---
+++ Users.APP/Migrations/20251217103654_seedData.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Users.APP.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Guid",
+                table: "UserMovies",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "UserMovies",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "MovieName",
+                table: "UserMovies",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.InsertData(
+                table: "Groups",
+                columns: new[] { "Id", "Guid", "Title" },
+                values: new object[,]
+                {
+                    { 1, null, "Child" },
+                    { 2, null, "Adult" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "Guid", "Name" },
+                values: new object[,]
+                {
+                    { 1, null, "Admin" },
+                    { 2, null, "Customer" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Groups",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Groups",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DropColumn(
+                name: "Guid",
+                table: "UserMovies");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "UserMovies");
+
+            migrationBuilder.DropColumn(
+                name: "MovieName",
+                table: "UserMovies");
+        }
+    }
+}
---
+++ Users.APP/Migrations/20251217112720_change.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Users.APP.Migrations
+{
+    /// <inheritdoc />
+    public partial class change : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_CityId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_CountryId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "CityId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "CountryId",
+                table: "Users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CityId",
+                table: "Users",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CountryId",
+                table: "Users",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_CityId",
+                table: "Users",
+                column: "CityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_CountryId",
+                table: "Users",
+                column: "CountryId");
+        }
+    }
+}
